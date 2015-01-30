05b261abb72c79380f191e762daa0697b8ec2711
Rob Zimmerman <taintedz@gmail.com>
2015-01-30 12:53:18 -0500

    Show CodeSchool Courses
    Grabs the courses dynamically from the CodeSchool API.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log('This would be the main JS file.');
+jQuery(document).ready ( function() {
+  jQuery.get( 'https://www.codeschool.com/users/taintedzodiac.json', function(data) {
+    var user = data['user'];
+    var courses = data['courses'];
+    var badges = data['badges'];
+
+    $('#codeschool-courses').html('<div id="completed"><h3>Completed</h3></div>'
+    + '<div id="in_progress"><h3>In Progress</h3></div>');
+
+    jQuery.each(['completed', 'in_progress'], function(i, category) {
+      jQuery.each(courses[category], function(j, course) {
+        jQuery('#' + category).append(
+          '<a href="'
+          + course['url']
+          + '"><img src="'
+          + course['badge']
+          + '" width="20%" /></a>'
+        );
+      });
+    });
+  },
+  'jsonp');
+});
