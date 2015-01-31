e7154d20ac6c169f1b44ce69bd72e27ffbd1fca9
Rob Zimmerman <taintedz@gmail.com>
2015-01-30 19:17:12 -0500

    Move badges to sidebar

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,17 +4,17 @@ jQuery(document).ready ( function() {
     var courses = data['courses'];
     var badges = data['badges'];
 
-    $('#codeschool-courses').html('<div id="completed"><h3>Completed</h3></div>'
-    + '<div id="in_progress"><h3>In Progress</h3></div>');
+    $('#codeschool-courses').html(
+      '<div id="in_progress"><h3>In Progress</h3></div>'
+      + '<div id="completed"><h3>Completed</h3></div>'
+      );
 
     jQuery.each(['completed', 'in_progress'], function(i, category) {
       jQuery.each(courses[category], function(j, course) {
         jQuery('#' + category).append(
-          '<a href="'
-          + course['url']
-          + '"><img src="'
-          + course['badge']
-          + '" width="20%" /></a>'
+          '<a href="' + course['url'] + '">'
+          + '<img src="' + course['badge'] + '" width="75%" />'
+          + '</a><br />'
         );
       });
     });
